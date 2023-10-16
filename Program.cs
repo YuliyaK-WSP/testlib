08c8b59922b390643a75ca23ceda2a34842f0531
yulia <koroleva21.jv@gmail.com>
2023-10-16 22:13:57 +0300

    fix

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ds.test.impl;
+
+namespace testlib 
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            IPlugin plugin = Plugins.GetPlugin("AdditionPlugin");
+            if (plugin != null)
+            {
+                int result = plugin.Run(5, 3); 
+                Console.WriteLine("Результат: " + result);
+            }
+        }
+    }
+}
